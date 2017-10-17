9e4e9c35beb734513d0fae43c9ffbd88afdf393e
Admin <admin@Mac-Admin.local>
2017-10-17 15:36:23 +0300

    done

--- Weather/ViewControllers/SecondViewController.swift
+++ Weather/ViewControllers/SecondViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Kingfisher
 
 class SecondViewController: UIViewController {
-    @IBOutlet weak var test: StatusButton! 
+    @IBOutlet weak var test: StatusButton!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherImage: UIImageView!
     @IBOutlet weak var tempLabel: UILabel!
@@ -31,14 +31,4 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
